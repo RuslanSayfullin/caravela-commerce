8d51c99186ab1b3092002cbdf3f400019ad2754b
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-03-15 17:59:50 +0500

    Валидация форм с django-localflavor

--- caravela/caravela/settings.py
+++ caravela/caravela/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'rosetta',
+    'localflavor',
 ]+['shop.apps.ShopConfig',
    'cart.apps.CartConfig',
    'orders.apps.OrdersConfig',
