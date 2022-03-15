3a976915759d0e9eac6423d74f061f2f2ba495fa
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-03-15 17:55:47 +0500

    Отключили parler

--- caravela/caravela/settings.py
+++ caravela/caravela/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'rosetta',
-    'parler',
 ]+['shop.apps.ShopConfig',
    'cart.apps.CartConfig',
    'orders.apps.OrdersConfig',
@@ -98,21 +97,6 @@ LANGUAGES = (
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale/'),
 )
-
-# конфигурация перевода django-parler
-PARLER_LANGUAGES = {
-    None: (
-        {'code': 'en'},
-        {'code': 'es'},
-        {'code': 'ru'},
-    ),
-    'default': {
-        'fallback': 'ru',
-        'hide_untranslated': False,
-    }
-}
-# /конфигурация перевода django-parler
-
 # /Internationalization
 
 TIME_ZONE = 'UTC'
--- caravela/shop/models.py
+++ caravela/shop/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 from django.urls import reverse
-from parler.models import TranslatableModel, TranslatedFields
 
 
-class Category(TranslatableModel):
+class Category(models.Model):
     """Категория товаров в магазине"""
-    translations = TranslatedFields(
-        name=models.CharField(max_length=200, db_index=True),
-        slug=models.SlugField(max_length=200, db_index=True, unique=True))
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        #ordering = ('id', 'name',)
+        ordering = ('id',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -21,25 +19,23 @@ class Category(TranslatableModel):
         return reverse('shop:product_list_by_category', args=[self.slug])
 
 
-class Product(TranslatableModel):
+class Product(models.Model):
     """Описание модели товара"""
-    translations = TranslatedFields(
-        name=models.CharField(max_length=200, db_index=True),
-        slug=models.SlugField(max_length=200, db_index=True),
-        description=models.TextField(blank=True)
-    )
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        #ordering = ('id', 'name',)
+        ordering = ('id',)
         verbose_name = 'product'
         verbose_name_plural = 'products'
-        #index_together = (('id', 'slug'),)
+        index_together = (('id', 'slug'),)
 
     def __str__(self):
         return self.name
