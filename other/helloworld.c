6b1161739c112113468d7f9c8bae161d9c9f60e6
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-02-26 00:01:47 +0500

    Other

--- other/helloworld.c
+++
@@ -1,8 +0,0 @@
-//this is a comment
-#include <stdio.h>    //including header file in our program
-
-int main()            //main() where the execution begins
-{
-    printf("Hello World");
-    return 0;
-}
