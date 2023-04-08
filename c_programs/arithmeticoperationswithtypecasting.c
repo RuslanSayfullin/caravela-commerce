8992f254e97a10f20c756050242bb6d47ecd968b
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-04-09 00:05:50 +0500

    Arithmetic Operations with Typecasting

---
+++ c_programs/arithmeticoperationswithtypecasting.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+
+    int a, b, add, subtract, multiply, remainder;
+    float divide;
+
+    printf("Enter two integers: \n");
+    scanf("%d%d", &a, &b);
+
+    add = a+b;
+    subtract = a-b;
+    multiply = a*b;
+    divide = a/(float)b;
+    remainder = a%b;
+
+    printf("\n\nAddition of the numbers = %d\n", add);
+    printf("\nSubtraction of 2nd number from 1st = %d\n", subtract);
+    printf("\nMultiplication of the numbers = %d\n", multiply);
+    printf("\nDividing 1st number from 2nd = %f\n", divide);
+    printf("\nRemainder on Dividing 1st number by 2nd is %d\n", remainder);
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
