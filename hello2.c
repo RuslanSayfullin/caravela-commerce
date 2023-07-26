540f73a0dd06a36d074533ba57e80c08818d3a5d
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-07-26 10:18:59 +0500

    Разработка первой программы на C

---
+++ hello2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    if (puts("Hello, world!") == EOF) {
+        return EXIT_FAILURE;
+        // здесь код никогда не выполняется
+    }
+    return EXIT_SUCCESS;
+    // здесь код никогда не выполняется
+}
